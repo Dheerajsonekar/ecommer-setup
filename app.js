695b76c2b1a2ca727a79b23e3d340426dd3840f6
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-13 07:56:26 +0530

    task

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const productRoutes = require('./routes/productRoutes');
 
+// Middleware
 app.use(express.json());
 
-// Product routes
-app.use(productRoutes);
+// Serve HTML file from /VIEW on /api/products/html
+app.get('/api/products/html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'VIEW', 'products.html'));
+});
 
+// All other routes
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
 
+app.use('/users', userRoutes);
+app.use('/products', productRoutes);
+app.use('/cart', cartRoutes);
 
-app.listen(4000, () => {
-    console.log("Server is running on http://localhost:4000");
+// 404 handler
+app.use((req, res) => {
+    res.status(404).send("Page not found");
+});
+
+// Start server
+const PORT = 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
