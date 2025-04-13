d1fc147ec5f629271f3ffe3ceae9deb022fe0413
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-14 05:17:07 +0530

    database connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const productRoutes = require('./routes/productRoutes');
+const database = require('./db.js');
 
 // Middleware for parsing form data (from Axios POST)
 app.use(express.json());
@@ -9,7 +10,17 @@ app.use(express.urlencoded({ extended: true }));
 // Set up routes
 app.use('/api/products', productRoutes);
 
-const PORT = 3000;
+database.connect((err)=>{
+  if(err){
+    console.log("error is connection ", err);
+  }
+  else{
+    console.log("connected to database");
+    const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+  }
+})
+
+
