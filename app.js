24d4c886101e91a1c1736b4f14a084929a8844ae
Dheerajsonekar <dheerajsonekar2@gmail.com>
2025-04-13 07:42:04 +0530

    controllers

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const app = express();
-
-// Import Routes
-const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
-const cartRoutes = require('./routes/cartRoutes');
 
-// Middleware for parsing JSON bodies
 app.use(express.json());
 
-// Use Routes
-app.use('/users', userRoutes);
-app.use('/products', productRoutes);
-app.use('/cart', cartRoutes);
+// Product routes
+app.use(productRoutes);
 
-// Start server
+
+
 app.listen(4000, () => {
     console.log("Server is running on http://localhost:4000");
 });
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,19 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const productController = require('../controllers/productController');
 
-// GET /products - Fetch all products
-router.get('/', (req, res) => {
-    res.send("Fetching all products");
-});
-
-// POST /products - Add a new product
-router.post('/', (req, res) => {
-    res.send("Adding a new product");
-});
-
-// GET /products/:id - Fetch a product by its ID
-router.get('/:id', (req, res) => {
-    res.send(`Fetching product with ID: ${req.params.id}`);
-});
+router.get('/products', productController.getAllProducts);
+router.get('/products/:id', productController.getProductById);
+router.post('/products', productController.addNewProduct);
 
 module.exports = router;
